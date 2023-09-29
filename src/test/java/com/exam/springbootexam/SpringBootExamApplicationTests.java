ad4323805cd6e95eea7a064d514a382afe66e002
Fanjasoa Andriamanjaka <hei.fanjasoa@gmail.com>
2023-09-29 08:25:17 +0300

    chore: add dependencies

---
+++ src/test/java/com/exam/springbootexam/SpringBootExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam.springbootexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
